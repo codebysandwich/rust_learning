502fedf49e75ce53680a29311a1b3532556e1c5f
sandwich <122079260@qq.com>
2024-10-21 22:23:34 +0800

    add mut/reference and Association in struct

---
+++ src/reference/counter.rs
@@ -0,0 +1,52 @@
+/*
+* File       : counter.rs
+* Time       ：2024/10/21 21:56
+* Author     ：sandwich
+* version    ：V1.0
+* Description：
+*/
+
+struct Counter {
+    number: i32,
+}
+
+/// 实现结构体的关联函数
+impl Counter {
+    /// 关联函数,用以初始化新的对象
+    fn new(number: i32) -> Self {
+        Self { number }
+    }
+    /// 获取当前对象的不可变引用，访问数据
+    fn get_number(&self) -> i32 {
+        self.number
+    }
+    /// 通过当前对象的可变引用，修改数据
+    fn add(&mut self, increment: i32) {
+        self.number += increment;
+    }
+    /// 获取当前对象的所有权，并进行释放
+    fn free(self) {
+        println!("free counter number is {}", self.number);
+    }
+    /// 获取left与right的所有权并生成新的对象
+    fn combine(left: Self, right: Self) -> Self {
+        Self {
+            number: left.number + right.number
+        }
+    }
+}
+
+#[test]
+fn test_counter() {
+    let mut count = Counter::new(0);
+    println!("count number is {}", count.get_number());
+    // 修改数据
+    count.add(10);
+    println!("count number is {}", count.get_number());
+    count.free(); // 交出所有权
+    // println!("count number is {}", count.get_number()); // borrowed
+    let c1 = Counter::new(3);
+    let c2 = Counter::new(5);
+    let c3 = Counter::combine(c1, c2);
+    println!("count number is {}", c3.get_number());
+}
---
+++ src/reference/mod.rs
@@ -0,0 +1,9 @@
+/*
+* File       : mod.rs
+* Time       ：2024/10/21 22:01
+* Author     ：sandwich
+* version    ：V1.0
+* Description：
+*/
+
+pub mod counter;
